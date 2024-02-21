c10dbdcdc4a884469cb1bb3333bc997856c05950
Andrea Bazzano <andrea.bazzano@arpal.liguria.it>
2024-02-21 16:49:54 +0100

    measurement modules dynamically added

--- R/app_server.R
+++ R/app_server.R
@@ -3,9 +3,53 @@
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @importFrom bslib nav_remove nav_insert
 #' @noRd
 app_server <- function(input, output, session) {
-  options(scipen = 999)
+  options(scipen = 999,
+          DT.options = list(pageLength = 11,
+                            dom = 't',
+                            ordering = FALSE))
 
   r <- reactiveValues()
+
+  mod_01_device_server("scale", r)
+  mod_02_method_server("calibration", r)
+  mod_03_environment_server("environment", r)
+
+
+  ##### dinamically remove and add measurement panels ----
+  observeEvent(r$scale$nint, {
+
+    # remove the measurement panels
+    lapply(1:2, function(x) {
+
+      valuepanel <- paste0("range", x)
+
+      bslib::nav_remove(
+        id = "mainmenu",
+        target = valuepanel,
+    )
+    })
+
+    npanel <- r$scale$nint |> as.numeric()
+
+    lapply(npanel:1, function(x) {
+
+      namepanel <- paste0("Intervallo ", x)
+      valuepanel <- paste0("range", x)
+
+      # adding the measurement panels
+      bslib::nav_insert(
+        id = "mainmenu",
+        target = "environment",
+        select = FALSE,
+        bslib::nav_panel(namepanel, value = valuepanel, mod_04_measure_ui(valuepanel))
+      )
+
+      # adding the server modules
+      mod_04_measure_server(valuepanel, r)
+    })
+
+  })
 }
