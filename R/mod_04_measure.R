ecfda42f5c55ecbd020617a8fe1a898b7fe47948
Andrea Bazzano <andrea.bazzano@arpal.liguria.it>
2024-02-22 15:05:37 +0100

    first output draft: check warning message

--- R/mod_04_measure.R
+++ R/mod_04_measure.R
@@ -11,12 +11,12 @@
 #'
 #' @import shiny
 #' @importFrom bslib card card_header card_body navset_card_tab nav_panel layout_column_wrap
+#' @importFrom glue glue
 mod_04_measure_ui <- function(id) {
   ns <- NS(id)
   tagList(
 
     bslib::layout_columns(
-      width = 2,
 
     numericInput(ns("minfield"), "Minimo del campo di utilizzo (g):", value = 0, min = 0),
     numericInput(ns("maxfield"), "Massimo del campo di utilizzo (g):", value = 0, min = 0),
@@ -27,14 +27,51 @@ mod_04_measure_ui <- function(id) {
     ),
 
     bslib::navset_card_tab(
-      height = 500,
+      height = 600,
 
     bslib::nav_panel(
       fillable = FALSE,
       title = "Eccentricità",
 
-      DT::DTOutput(ns("eccentricity")),
-      htmlOutput(ns("eccresult"))
+      bslib::layout_columns(
+        col_widths = c(6, 3, 3),
+
+        bslib::card(
+          bslib::card_body(
+            numericInput(ns("eccload"), "Massa utilizzata (g):", value = 0, min = 0),
+
+            DT::DTOutput(ns("eccentricity")),
+            htmlOutput(ns("eccresult"))
+          )
+        ),
+
+        bslib::card(
+          bslib::card_body(
+
+            shiny::tags$img(src = "www/scale_scheme.png",
+                            style = "height:100%;
+                                     width:auto;
+                                     display:block;
+                                     margin-left:auto;
+                                     margin-right:auto;")
+          )
+        ),
+
+        bslib::card(
+          bslib::card_header(shiny::icon("circle-info"), "Cosa devi fare"),
+          bslib::card_body(
+
+            list(one = "Compila i campi sovrastanti;",
+                 two = "carica la massa nelle posizioni indicate nello schema;",
+                 three = "fai doppio click nella tabella per inserire le letture;",
+                 four = "una volta completato l'inserimento, conferma con Ctrl + Invio.") |>
+              list_to_li()
+          )
+        )
+
+      )
+
+
     ),
 
     bslib::nav_panel(
@@ -82,21 +119,27 @@ mod_04_measure_server <- function(id, r){
 
 
     # eccentricity table
-    output$eccentricity <- DT::renderDT(eccrv$data,
-                                         style = "bootstrap",
-                                         selection = "none",
-                                         colnames = c("Posizione", "Lettura (g)", "Differenza rispetto alla posizione 1 (g)"),
-                                         rownames = FALSE,
-                                         editable = list(target = "column",
-                                                         numeric = 2,
-                                                         disable = list(columns = c(0, 2)))
+    output$eccentricity <- DT::renderDT(
+      DT::datatable(eccrv$data,
+                    style = "bootstrap",
+                    selection = "none",
+                    colnames = c("Posizione", "Lettura (g)", "Differenza rispetto alla posizione 1 (g)"),
+                    rownames = FALSE,
+                    editable = list(
+                      target = "column",
+                      numeric = c(1, 2),
+                      disable = list(columns = c(0, 2))
+                      )
+                    ) |>
+        DT::formatRound(c(2, 3), digits = r$scale$signifdigits)
     )
 
     # when the table is edited the differences are computed
     observeEvent(input$eccentricity_cell_edit, {
 
       eccrv$data <- DT::editData(eccrv$data, input$eccentricity_cell_edit, "eccentricity", rownames = FALSE)
-      eccrv$data$DI <- abs(eccrv$data$lettura - eccrv$data$lettura[1]) |> round(r$scale$signifdigits)
+      numread <- eccrv$data$lettura |> as.numeric()
+      eccrv$data$DI <- abs(numread - numread[1]) |> round(r$scale$signifdigits)
 
     })
 
@@ -108,30 +151,56 @@ mod_04_measure_server <- function(id, r){
 
     # max difference due to eccentricity
     eccresult <- reactive({
-      req(ecclength() == 5)
 
       maxdiff <- max(eccrv$data$DI)
-      unc <- (maxdiff/(2 * sqrt(3))) |>
-        mysigformat(r$scale$signifdigits)
+      unc <- (maxdiff/(2 * sqrt(3))) |> mysigformat(r$scale$signifdigits)
+      mycol <- ifelse(maxdiff > 3 * input$gsd, "text-warning", "text-success")
+      myres <- ifelse(maxdiff > 3 * input$gsd, "non conforme", "conforme")
+      mytext <- if(ecclength() < 5) {
+        "Servono 5 misure." |> mywarning()
+      } else if (input$eccload > input$maxcal) {
+        "La massa impiegata non può eccedere il massimo dell'intervallo di taratura." |> mywarning()
+      } else if (input$eccload < input$mincal) {
+        "La massa impiegata non può essere inferiore al minimo dell'intervallo di taratura." |> mywarning()
+      } else if (input$maxcal > input$maxfield) {
+        "Il massimo dell'intervallo di taratura non può eccedere il massimo del campo di utilizzo." |> mywarning()
+      } else if (input$mincal < input$minfield) {
+        "Il minimo dell'intervallo di taratura non può essere inferiore al minimo del campo di utilizzo." |> mywarning()
+      } else {
+        glue::glue(
+          "<ul>
+              <li class = {mycol}> esito: {myres} </li>
+              <li> differenza massima = {maxdiff} g </li>
+              <li> contributo all'incertezza u(e) = {unc} g </li>
+          </ul>")
+      }
+      myplaintext <- if(ecclength() < 5) {
+        "Servono 5 misure."
+      } else if (input$eccload > input$maxcal) {
+        "La massa impiegata non può eccedere il massimo dell'intervallo di taratura."
+      } else if (input$eccload < input$mincal) {
+        "La massa impiegata non può essere inferiore al minimo dell'intervallo di taratura."
+      } else if (input$maxcal > input$maxfield) {
+        "Il massimo dell'intervallo di taratura non può eccedere il massimo del campo di utilizzo."
+      } else if (input$mincal < input$minfield) {
+        "Il minimo dell'intervallo di taratura non può essere inferiore al minimo del campo di utilizzo."
+      } else {
+        glue::glue(
+               "* esito: {myres}
+                * differenza massima = {maxdiff} g
+                * contributo all'incertezza u(e) = {unc} g"
+               )
+      }
 
       list(maxdiff = maxdiff,
            uncecc = unc,
-           result = ifelse(maxdiff > 3 * input$gsd,
-                           "non conforme",
-                           "conforme"
+           result = mytext,
+           plainresult = myplaintext
            )
-      )
-
     })
 
 
-    output$eccresult <- renderText({
-
-      eccresult()$result
-    })
-
-
-
+    output$eccresult <- renderText(eccresult()$result)
 
 
     #### saving all the inputs ----
@@ -143,6 +212,17 @@ mod_04_measure_server <- function(id, r){
       lapply(savename, function (x) {
         r[[mynamespace]][[x]] <- input[[x]]
       })
+
+      #### saving the output ----
+      resultname <- names(eccresult())
+
+      lapply(resultname, function (x) {
+        r[[mynamespace]][[x]] <- eccresult()[[x]]
+      })
+
+      print(r[[mynamespace]])
+      print(r[[mynamespace]]$result)
+      print(output)
 
     })
 
