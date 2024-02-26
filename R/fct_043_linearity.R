49fc6f0bbc79d391672b1d522cd2a06bdf144ab1
Andrea Bazzano <andrea.bazzano@arpal.liguria.it>
2024-02-26 16:13:35 +0100

    functions for linearity test

---
+++ R/fct_043_linearity.R
@@ -0,0 +1,39 @@
+#' Linearity table
+#'
+#' @description A DT for linearity results
+#'
+#' @param df a dataframe with three columns to be included in the DT.
+#' @param mydigits the significant digits for scale readings.
+#' @return a DT table.
+#' @importFrom DT datatable formatRound
+#' @noRd
+DTlinearity <- function(df, mydigits) {
+  stopifnot(
+    is.data.frame(df),
+    is.numeric(mydigits),
+    dim(df)[2] == 9
+  )
+
+  DT::datatable(df,
+                style = "bootstrap",
+                selection = "none",
+                colnames = c("Prova",
+                             "Valore nominale (g)",
+                             "Valore convenzionale (g)",
+                             "Lettura (g)",
+                             "Errore (g)",
+                             "Prova",
+                             "Lettura (g)",
+                             "Errore (g)",
+                             "Errore medio (g)"
+                ),
+                rownames = FALSE,
+                fillContainer = TRUE,
+                editable = list(target = "column",
+                                numeric = 0:8,
+                                disable = list(columns = c(0, 4, 5, 7, 8))
+                )
+  ) |> DT::formatRound(c(5, 8, 9), digits = mydigits + 1)
+}
+
+
